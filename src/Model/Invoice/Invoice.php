586ceb17bb78852433c840cca7d3df89c7423f2c
Jakub Piasecki <jakub@piaseccy.pl>
2017-07-18 10:04:11 +0200

    Add getters to invoice model

--- src/Model/Invoice/Invoice.php
+++ src/Model/Invoice/Invoice.php
@@ -18,16 +18,16 @@ class Invoice implements ModelInterface
     protected $invoiceNumber; // (integer, optional): If value is set to 0, the invoice number will be generated. ,
 
     /**
-     * @var string
+     * @var \DateTimeInterface
      *
-     * @Serializer\Type("string")
+     * @Serializer\Type("DateTime<'Y-m-d'>")
      */
     protected $invoiceDate; // (string),
 
     /**
-     * @var string
+     * @var \DateTimeInterface
      *
-     * @Serializer\Type("string")
+     * @Serializer\Type("DateTime<'Y-m-d'>")
      */
     protected $invoiceDueDate; // (string),
 
@@ -60,9 +60,9 @@ class Invoice implements ModelInterface
     protected $voucher; // (Voucher, optional, read only): The invoice voucher. ,
 
     /**
-     * @var string
+     * @var \DateTimeInterface
      *
-     * @Serializer\Type("string")
+     * @Serializer\Type("DateTime<'Y-m-d'>")
      */
     protected $deliveryDate; // (string, optional, read only): The delivery date. ,
 
@@ -114,4 +114,248 @@ class Invoice implements ModelInterface
      * @Serializer\Type("string")
      */
     protected $ehfSendStatus; // (string, optional) = ['DO_NOT_SEND', 'SEND', 'SENT', 'SEND_FAILURE_RECIPIENT_NOT_FOUND']
+
+    /**
+     * Gets kid value.
+     *
+     * @return string
+     */
+    public function getKid()
+    {
+        return $this->kid;
+    }
+
+    /**
+     * Sets kid variable.
+     *
+     * @param string $kid
+     *
+     * @return $this
+     */
+    public function setKid($kid)
+    {
+        $this->kid = $kid;
+        return $this;
+    }
+
+    /**
+     * Gets amountExcludingVatCurrency value.
+     *
+     * @return float
+     */
+    public function getAmountExcludingVatCurrency()
+    {
+        return $this->amountExcludingVatCurrency;
+    }
+
+    /**
+     * Gets currency value.
+     *
+     * @return \zaporylie\Tripletex\Model\Currency\Currency
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * Gets deliveryDate value.
+     *
+     * @return \DateTimeInterface
+     */
+    public function getDeliveryDate()
+    {
+        return $this->deliveryDate;
+    }
+
+    /**
+     * Gets invoiceDate value.
+     *
+     * @return \DateTimeInterface
+     */
+    public function getInvoiceDate()
+    {
+        return $this->invoiceDate;
+    }
+
+    /**
+     * Sets invoiceDate variable.
+     *
+     * @param \DateTimeInterface $invoiceDate
+     *
+     * @return $this
+     */
+    public function setInvoiceDate(\DateTimeInterface $invoiceDate)
+    {
+        $this->invoiceDate = $invoiceDate;
+        return $this;
+    }
+
+    /**
+     * Gets amount value.
+     *
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+
+    /**
+     * Gets amountCurrency value.
+     *
+     * @return float
+     */
+    public function getAmountCurrency()
+    {
+        return $this->amountCurrency;
+    }
+
+    /**
+     * Gets amountExcludingVat value.
+     *
+     * @return float
+     */
+    public function getAmountExcludingVat()
+    {
+        return $this->amountExcludingVat;
+    }
+
+    /**
+     * Gets comment value.
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Sets comment variable.
+     *
+     * @param string $comment
+     *
+     * @return $this
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+        return $this;
+    }
+
+    /**
+     * Gets ehfSendStatus value.
+     *
+     * @return string
+     */
+    public function getEhfSendStatus()
+    {
+        return $this->ehfSendStatus;
+    }
+
+    /**
+     * Sets ehfSendStatus variable.
+     *
+     * @param string $ehfSendStatus
+     *
+     * @return $this
+     */
+    public function setEhfSendStatus($ehfSendStatus)
+    {
+        $this->ehfSendStatus = $ehfSendStatus;
+        return $this;
+    }
+
+    /**
+     * Gets invoiceDueDate value.
+     *
+     * @return \DateTimeInterface
+     */
+    public function getInvoiceDueDate()
+    {
+        return $this->invoiceDueDate;
+    }
+
+    /**
+     * Sets invoiceDueDate variable.
+     *
+     * @param \DateTimeInterface $invoiceDueDate
+     *
+     * @return $this
+     */
+    public function setInvoiceDueDate(\DateTimeInterface $invoiceDueDate)
+    {
+        $this->invoiceDueDate = $invoiceDueDate;
+        return $this;
+    }
+
+    /**
+     * Gets invoiceNumber value.
+     *
+     * @return int
+     */
+    public function getInvoiceNumber()
+    {
+        return $this->invoiceNumber;
+    }
+
+    /**
+     * Sets invoiceNumber variable.
+     *
+     * @param int $invoiceNumber
+     *
+     * @return $this
+     */
+    public function setInvoiceNumber($invoiceNumber)
+    {
+        $this->invoiceNumber = $invoiceNumber;
+        return $this;
+    }
+
+    /**
+     * Gets orders value.
+     *
+     * @return \zaporylie\Tripletex\Model\Order\Order[]
+     */
+    public function getOrders()
+    {
+        return $this->orders;
+    }
+
+    /**
+     * Sets orders variable.
+     *
+     * @param \zaporylie\Tripletex\Model\Order\Order[] $orders
+     *
+     * @return $this
+     */
+    public function setOrders($orders)
+    {
+        $this->orders = $orders;
+        return $this;
+    }
+
+    /**
+     * Gets voucher value.
+     *
+     * @return \zaporylie\Tripletex\Model\Ledger\Voucher
+     */
+    public function getVoucher()
+    {
+        return $this->voucher;
+    }
+
+    /**
+     * Gets isCreditNote value.
+     *
+     * @return bool
+     */
+    public function getIsCreditNote()
+    {
+        return $this->isCreditNote;
+    }
+
+
 }
