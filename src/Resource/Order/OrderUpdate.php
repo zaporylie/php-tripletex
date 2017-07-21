f9ecc66dc85e47c74f22103d7325c841e258c990
Jakub Piasecki <jakub@piaseccy.pl>
2017-07-21 11:03:38 +0200

    Fix order update

--- src/Resource/Order/OrderUpdate.php
+++ src/Resource/Order/OrderUpdate.php
@@ -20,7 +20,7 @@ class OrderUpdate extends ResourceBase implements ResourceInterface
     /**
      * @var string
      */
-    protected $path = '/order';
+    protected $path = '/order/{id}';
 
     /**
      * @param \zaporylie\Tripletex\Model\Order\Order $requestObject
@@ -32,10 +32,8 @@ class OrderUpdate extends ResourceBase implements ResourceInterface
         /** @var \Psr\Http\Message\RequestInterface $request */
         $request = $this->app->getClient()->messageFactoryDiscovery()->createRequest(
             $this->getMethod(),
-            $this->getPath().'/'.$requestObject->getId().'?'.http_build_query([
-                'fields' => $requestObject->getFields(),
-            ]),
-            [],
+            $this->getPath($requestObject->getId()),
+            ['Content-Type' => 'application/json; charset=utf-8'],
             $this->app->getSerializer()->serialize($requestObject, 'json')
         );
         /** @var \Psr\Http\Message\ResponseInterface $response */
