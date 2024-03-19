f9aa5d3320e39f181afd13e67213b0532bdd7a2a
Eirik S. Morland <eirik@morland.no>
2024-03-19 12:26:33 +0100

    Add methods for customer on contact

--- src/Model/Contact/Contact.php
+++ src/Model/Contact/Contact.php
@@ -3,6 +3,7 @@
 namespace zaporylie\Tripletex\Model\Contact;
 
 use JMS\Serializer\Annotation as Serializer;
+use zaporylie\Tripletex\Model\Customer\Customer;
 use zaporylie\Tripletex\Model\ModelInterface;
 use zaporylie\Tripletex\Model\ModelTrait;
 
@@ -37,4 +38,97 @@ class Contact implements ModelInterface
      * @Serializer\Type("zaporylie\Tripletex\Model\Customer\Customer")
      */
     protected $customer; // (Customer, optional)
+
+    /**
+     * Gets first name.
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param string $first_name
+     *
+     * @return $this
+     */
+    public function setFirstName($first_name)
+    {
+        $this->firstName = $first_name;
+        return $this;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param string $last_name
+     *
+     * @return $this
+     */
+    public function setLastName($last_name)
+    {
+        $this->lastName = $last_name;
+        return $this;
+    }
+
+    /**
+     * Gets email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Sets email
+     *
+     * @param string $email
+     *
+     * @return $this
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * Gets customer.
+     *
+     * @return \zaporylie\Tripletex\Model\Customer\Customer
+     */
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    /**
+     * Sets customer.
+     *
+     * @param \zaporylie\Tripletex\Model\Customer\Customer $customer
+     *
+     * @return $this
+     */
+    public function setCustomer(Customer $customer)
+    {
+        $this->customer = $customer;
+        return $this;
+    }
+
 }
